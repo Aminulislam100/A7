4828776e9e3f692c5d4e05c75d017ece7b920f48
Aminulislam100 <aminulislamittl@gmail.com>
2025-04-27 20:46:42 +0600

    dfgid

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,24 +1,26 @@
 import React from 'react';
-import banner from '../images/Banner-min.jpg';
 
 const Hero = () => {
     return (
         <div
-  className="hero h-130"
-  style={{
-    backgroundImage: `url(${banner})`,
-  }}>
-  <div className="hero-overlay"></div>
-  <div className="hero-content text-neutral-content -ml-190">
-    <div className="max-w-md">
-      <h1 className="mb-5 text-4xl font-medium">Bid on Unique Items from Around the World</h1>
-      <p className="mb-5">
-      Discover rare collectibles, luxury goods, and vintage treasures in our curated auctions
-      </p>
-      <button className="btn rounded-full">Explore Auctions</button>
-    </div>
-  </div>
-</div>
+          className="hero h-130"
+          style={{
+            backgroundImage: `url(/images/Banner-min.jpg)`,
+          }}
+        >
+          <div className="hero-overlay"></div>
+          <div className="hero-content text-neutral-content -ml-190">
+            <div className="max-w-md">
+              <h1 className="mb-5 text-4xl font-medium">
+                Bid on Unique Items from Around the World
+              </h1>
+              <p className="mb-5">
+                Discover rare collectibles, luxury goods, and vintage treasures in our curated auctions
+              </p>
+              <button className="btn rounded-full">Explore Auctions</button>
+            </div>
+          </div>
+        </div>
     );
 };
 
